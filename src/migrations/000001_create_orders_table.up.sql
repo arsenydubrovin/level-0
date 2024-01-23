d2a132d60406be80920c70caf8b94819b3c6c60a
Arseny Dubrovin <arsenydubrovin@arsenydubrovin.ru>
2024-01-23 11:54:42 +0300

    Add "uid" column to orders

--- src/migrations/000001_create_orders_table.up.sql
+++ src/migrations/000001_create_orders_table.up.sql
@@ -1,5 +1,6 @@
 CREATE TABLE IF NOT EXISTS orders (
     id bigserial PRIMARY KEY,
+    uid CHAR(19) UNIQUE NOT NULL,
     data jsonb NOT NULL,
     created_at timestamp(0) with time zone NOT NULL DEFAULT NOW()
 );
