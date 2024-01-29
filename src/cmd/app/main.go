44e4d1e9080e588785335d9a59a67f9d043e491b
Arseny Dubrovin <arsenydubrovin@arsenydubrovin.ru>
2024-01-29 12:24:24 +0300

    Add graceful shutdown

--- src/cmd/app/main.go
+++ src/cmd/app/main.go
@@ -3,7 +3,11 @@ package main
 import (
 	"context"
 	"log"
+	"log/slog"
 	"net/http"
+	"os"
+	"os/signal"
+	"time"
 
 	"github.com/arsenydubrovin/level-0/src/internal/app"
 )
@@ -16,10 +20,26 @@ func main() {
 		log.Fatalf("failed to init app: %s", err.Error())
 	}
 
-	err = a.Run()
-	if err != nil && err != http.ErrServerClosed {
-		log.Fatalf("failed to run app: %s", err.Error())
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer stop()
+
+	go func() {
+		err = a.Run()
+		if err != nil && err != http.ErrServerClosed {
+			log.Fatalf("shutting down the app: %s", err.Error())
+		}
+	}()
+
+	<-ctx.Done()
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	slog.Debug("completing background tasks...")
+
+	if err := a.Stop(ctx); err != nil {
+		log.Fatalf("failed to init app: %s", err.Error())
 	}
 
-	// TODO: gracefull shutdown
+	slog.Debug("shutting down the app...")
 }
